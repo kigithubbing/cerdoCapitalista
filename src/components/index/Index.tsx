8f4d5de7248f9bf277a741fe346a71daf077df2d
Iñaki de Pablo Rodriguez <ing.inaki@gmail.com>
2024-04-22 21:07:58 -0600

    CSS work done to Index. Web icon was changed. Ordering of inside elements were also changed.

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom"
-import {FormProvider, useForm} from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 import './Index.css'
 import { useState } from "react"
 import { UserFormData } from "../../types/userTypes"
@@ -11,71 +11,77 @@ enum levels {
     High
 }
 
-function UnicodeAcceptedTest()  {
-try {
-    new RegExp('\\p{L}', 'u');
-    return true;
-  } catch (e) {
-    return false;
-  }
+function UnicodeAcceptedTest() {
+    try {
+        new RegExp('\\p{L}', 'u');
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
 function Index() {
-    const [user,createUser] = useUserDetailsStore((state) =>[
+    const [user, createUser] = useUserDetailsStore((state) => [
         state.user, state.createUser
-]);
+    ]);
 
     const browserAcceptsUnicode: boolean = UnicodeAcceptedTest();
-    const {register,handleSubmit,formState: {errors} } = useForm<UserFormData>();
-    const onSubmit= (data:UserFormData) => { 
+    const { register, handleSubmit, formState: { errors } } = useForm<UserFormData>();
+    const onSubmit = (data: UserFormData) => {
         console.log(data)
         setIsFormSubmitted(true)
         createUser(data)
-     }
-    
-    const [isFormSubmitted,setIsFormSubmitted] = useState (false);
+    }
+
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
     return (
         <>
-        {isFormSubmitted &&
-        <div>
-           <h2> Welcome {user.name}</h2>
-        </div>
-        }
-        <form onSubmit={handleSubmit(onSubmit)} className="centered-form">
-            <div >
-                <label htmlFor="name">Name:
-                    <input type="textbox"  {...register("name",{required:"Your name is required."
-                    ,pattern :  { value : browserAcceptsUnicode ? /^(\p{L}+((-\p{L}+)|\s\p{L}+|'\p{L}+)*){3,}$/iu :  /^[A-Z]{3,}((-[A-Z]+)|\s[A-Z]+)*$/i , message: "Name should be at least three characters long" }
-                     } ) } />
-                </label>
-                {errors.name  && <span>{errors.name.message}</span>}
+            {isFormSubmitted &&
+                <div>
+                    <h2> Welcome {user.name}</h2>
+                </div>
+            }
+            <div className="box">
+                <div className="center">
+                    <h1>Home of capitalist swines.</h1>
+                    <h4 style={{ marginTop: -30 }}>or those who want to become</h4>
+                    <img height={150} src="src/assets/piggy/il_fullxfull.1270738567_ms47.webp" alt="Bourbon" />
+                </div>
+                <div className="center">
+                    <form onSubmit={handleSubmit(onSubmit)} className="centered-form">
+                        <label htmlFor="name">Name:
+                            <input type="textbox"  {...register("name", {
+                                required: "Your name is required."
+                                , pattern: { value: browserAcceptsUnicode ? /^(\p{L}+((-\p{L}+)|\s\p{L}+|'\p{L}+)*){3,}$/iu : /^[A-Z]{3,}((-[A-Z]+)|\s[A-Z]+)*$/i, message: "Name should be at least three characters long" }
+                            })} />
+                        </label>
+                        {errors.name && <span>{errors.name.message}</span>}
+                            <label htmlFor="level">Level:
+                                <select {...register("level", { valueAsNumber: true, min: 1, required: "Choose a level" })}>
+                                    <option value="">Choose ...</option>
+                                    <option value={levels.Basic}>Basic</option>
+                                    <option value={levels.Medium}>Medium</option>
+                                    <option value={levels.High}>High</option>
+                                </select>
+                            </label>
+                            {errors.level && <span>{errors.level.message}</span>}
+                        <button type="submit" disabled={isFormSubmitted}>Go!</button>
+                    </form>
+                </div>
             </div>
-            <div>
-                <label htmlFor="level">Level:
-                    <select {...register("level",{ valueAsNumber:true, min:1, required:"Input"})}>
-                        <option value={levels.Unselected}>Choose ...</option>
-                        <option value={levels.Basic}>Basic</option>
-                        <option value={levels.Medium}>Medium</option>
-                        <option value={levels.High}>High</option>
-                    </select>
-                </label>
-                {errors.level && <span>{errors.level.message}</span>}
-            </div>
-            <button type="submit" disabled={isFormSubmitted}>Go!</button>
-        </form>
-        { isFormSubmitted  &&
-        <div>
-            <div >
-               <NavLink to={'/cryptoexchange'} > 
-                <p>Mercados Crypto</p>
-                </NavLink>
-            </div>
-            <div >
-            <NavLink to={'/exchange'} > 
-                <p>Mercado de Valores</p>
-                </NavLink>
-            </div>
-        </div>
-        }
+            {isFormSubmitted &&
+                <div>
+                    <div >
+                        <NavLink to={'/cryptoexchange'} >
+                            <p>Mercados Crypto</p>
+                        </NavLink>
+                    </div>
+                    <div >
+                        <NavLink to={'/exchange'} >
+                            <p>Mercado de Valores</p>
+                        </NavLink>
+                    </div>
+                </div>
+            }
         </>
     )
 }
